5c282b986d7bef6fd8bdd88c0c91f6637ac9e8c0
Thando Breyton Mamukeyani <110921825+Krimson100@users.noreply.github.com>
2024-08-22 16:26:44 -0400

    Add files via upload

---
+++ Aug-22.R
@@ -0,0 +1,12 @@
+#Author: Thando, Date:08/22/2024, Purpose:Test measures of descriptive stats
+
+# how to calculate standard deviation in R
+X <- c(1,2,3,4,5)
+sd(X)
+
+#how to calculate mean of X in R
+
+mean(X)
+
+#how to calculate median in R
+median(X)
